ba4bbdf760e554d22c39d60563338d121354552b
aiegoo <eozz21@gmail.com>
2019-08-25 11:23:38 +0900

    replace test2 folders and add boilerplate and html for test3 wedding 0903

--- test3/script/index.js
+++ test3/script/index.js
@@ -1,20 +1,5 @@
-$(".navs").hover(function(){
+$(".nav").hover(function(){
      $("ul", $(this)).stop().slideDown();
 }, function(){
      $("ul", $(this)).stop().slideUp();
 });
-
-var n = 0; 
-$(".btn-next").click(function(){
-     if (n < 2) n++;
-     ani();
-});
-
-$(".btn-prev").click(function(){
-     if (n > 0) n--;
-     ani();
-});
-
-function ani(){
-     $(".slide").stop().animate({"left": (-n*1000)+"px"}, 2000;
-}

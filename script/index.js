d6d299fa3df190c55409a04c26bf4389f0e27b4a
aiegoo <eozz21@gmail.com>
2019-08-25 18:32:21 +0900

    update css to adjust general layout and add js, needs click to work

--- script/index.js
+++ script/index.js
@@ -3,3 +3,18 @@ $(".nav").hover(function(){
 }, function(){
      $("ul", $(this)).stop().slideUp();
 });
+
+var n = 0;
+$(".btn-prev").click(function(){
+     if(n > 0) n--;
+     ani();
+});
+
+$(".btn-next").click(function(){
+     if(n < 2) n++;
+     ani();
+});
+
+function ani(){
+     $(".slide").stop().animate({"left": (-n*1000)+"px"}, 2000);
+}

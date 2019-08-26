75336a157d5296ccc86e72c29ca97d974fe6982c
aiegoo <eozz21@gmail.com>
2019-08-27 04:07:09 +0900

    add js to moving gallery 0903

---
+++ test5/scripts/index.js
@@ -0,0 +1,16 @@
+$(".nav").hover(function(){
+     $("ul", $(this)).stop().slideDown();
+}, function(){
+     $("ul", $(this)).stop().slideUp();
+});
+
+$(".bts").click(function(){
+     var n = $(this).index();
+     $(".bts").css("color", "#fff");
+     $(".bts").css("color", "#f30");
+     $(".slide").animate({"left": (-n*1000)+"px"}, 500);
+});
+
+function popUp(){
+     window.open("contact.html");
+}
